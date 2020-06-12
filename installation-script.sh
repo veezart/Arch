ef88cb17360684e1c5d24df5266e1e74b466632f
veezart <veezart@outlook.com>
2020-06-12 23:11:49 +0200

    Edit file

--- installation-script.sh
+++ installation-script.sh
@@ -4,6 +4,14 @@
 
 list=(
 
+# display server
+xorg
+xorg-xinit
+# display manager
+lightdm
+lightdm-gtk-greeter lightdm-gtk-greeter-settings
+# driver
+sudo pacman -S nvidia
 # install sound
 pulseaudio
 pulseaudio-alsa
@@ -18,10 +26,43 @@ gst-plugins-bad
 gst-plugins-base
 gst-plugins-ugly
 playerctl
-volumeicon
+# windows manager
+i3-gaps
+i3lock
+i3block
+# apperance
+lxappearance
+neofetch
+# tools
+xclip
+rofi
+htop
+mc
+pcmanfm-gtk3
+alacritty
+flameshot
+fille-roller
+xfce4-power-manager
+polkit
+# coompositor
+picom
+# common
+usbutils
+git
+
+
+
+
+
+
+
+
+
 )
 
 
 for name in "{list[@]}" ; do
 sudo pacman -S --noconfirm --needed $name
 done
+
+sudo systemctl enable lightdm.service
