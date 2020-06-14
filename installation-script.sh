73ad476ec8463501786d1fd7fc88150c91f8b8c3
veezart <66475813+veezart@users.noreply.github.com>
2020-06-14 14:28:37 +0200

    Update installation-script.sh

--- installation-script.sh
+++ installation-script.sh
@@ -4,44 +4,24 @@
 
 list=(
 # drivers
-nvidia
-intel-ucode
-btrfs-progs
-reiserfsprogs
-usbutils
-networkmanager
+
 # display server
-xorg
-xorg-xinit
+
 # display manager
-lightdm
-lightdm-gtk-greeter lightdm-gtk-greeter-settings
+
 # install sound
-pulseaudio
-pulseaudio-alsa
-pavucontrol
-alsa-firmware
-alsa-lib
-alsa-plugins
-alsa-utils
-gstreamer
-gst-plugins-good
-gst-plugins-bad
-gst-plugins-base
-gst-plugins-ugly
-playerctl
+
+
 # windows manager
-i3-gaps
-i3lock
-i3block
+
 # coompositor
-picom
+
 # apperance
 lxappearance
 neofetch
 # tools
 xclip
-rofi
+
 htop
 mc
 pcmanfm-gtk3
