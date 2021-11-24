6a3c0a7cbbabc180b6e39fe68f49e28e3c10ba90
veezart <veezart@outlook.com>
2021-11-24 03:42:31 +0100

    update

--- gnome.desktop.sh
+++ gnome.desktop.sh
@@ -1,23 +1,67 @@
 #!/bin/bash
 
+sudo pacman -S --noconfirm --needed git
+
+sudo pacman -S gnome
+
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
 list=(
-bash-comkpletion
-kitty
-ranger
-python-pillow #image preview
-xclip
-mc
-feh
-pcmanfm-gtk3
-file-roller
-polkit
-tree
-reflector
-htop
-arandr
+
+
+
+# drivers
+#--------
+    nvidia
+
+#tools
+#-----
+    bash-completions
+    feh
+    keepassxc
+    kitty
+    mpv
+    neofetch
+    reflector
+    tree
+    youtube-dl
+    zsh
+    zsh-completions
+
+#Fonts
+#-----
+    ttc-iosevka
+    ttf-fira-code
+    ttf-roboto
+    ttf-ubuntu-font-family
+    ttf-font-awesome
 )
+
+listyay=(
+
+    #papirus-folders-git
+    #ttf-inconsolata-g
+    dracula-gtk-theme
+    microsoft-edge-stable-bin
+)
+
 
 
 for name in "${list[@]}" ; do
 sudo pacman -S --noconfirm --needed $name
 done
+
+for name in "${listyay[@]}" ; do
+yay -S --noconfirm --needed $name
+done
+
+sudo systemctl enable gdm.service
+
+#zsh configuration
+zsh
+z
+touch ~/.zshrc
+chsh -s /bin/zsh
+
